b6c2d116710020ffa83bae3ba2e64f5f991f0f6d
Patrick_Roberto <patricktrp02@gmail.com>
2020-11-17 21:35:01 -0300

    Folder Build34

---
+++ frontEnd/build/precache-manifest.b7fdce16c40df2cb0d6eac7b41623a86.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8801fb7c5d5492c6d95c02b2a014311b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9da7ba06a6babbc93d4",
+    "url": "/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "1bedeeee6bf080c60de8",
+    "url": "/static/css/main.464df1a8.chunk.css"
+  },
+  {
+    "revision": "f9da7ba06a6babbc93d4",
+    "url": "/static/js/2.e95ebd1d.chunk.js"
+  },
+  {
+    "revision": "bd0419047791e8ffb605cc6b1cdf9fb1",
+    "url": "/static/js/2.e95ebd1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bedeeee6bf080c60de8",
+    "url": "/static/js/main.d78032b1.chunk.js"
+  },
+  {
+    "revision": "a600b51e8e64703e0b0d",
+    "url": "/static/js/runtime-main.945786e1.js"
+  }
+]);
